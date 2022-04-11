ce3d91d09dbb50e8f8c1bb462d73f570340e92fa
francois <francois.bourgeois@tuta.io>
2022-04-11 12:42:30 +0200

    logging debug retiré, id lifras en entier, date medic et ecg au format date

--- whois_lib.py
+++ whois_lib.py
@@ -4,7 +4,7 @@ import requests
 from datetime import datetime as dt
 import logging as log
 
-log.basicConfig(level=10)
+# log.basicConfig(level=10)
 
 
 def whois(nom, prenom="", dna="", id_lifras=""):
@@ -22,26 +22,26 @@ def whois(nom, prenom="", dna="", id_lifras=""):
 		"NrFederation2": id_lifras, 'Action':'Search','ClubID':'202','LG':'FR'}
 	
 	result = requests.post('https://www9.iclub.be/whois2.asp', headers=headers, data=data)
-	log.debug(result.text)
+	# log.debug(result.text)
 
 	if "No member founded" in result.text:
-		return {"status": False, "result":"No member founded"}
+		return {"status": False, "result":"Membre inconnu"}
 
 	form = result.text[result.text.index("<form"):result.text.index("</form>")]
 	form_lines = form.split("\n")
-	for seq, line in enumerate(form_lines):
-		log.debug(f"{seq}: {line}")
+	# for seq, line in enumerate(form_lines):
+	# 	log.debug(f"{seq}: {line}")
 	prenom = form_lines[2].split('value="')[1].split('"')[0]
 	nom = form_lines[4].split('value="')[1].split('"')[0]
 	dna = dt.strptime(form_lines[7].split('value="')[1].split('"')[0], "%d/%m/%Y").date()
-	id_lifras = form_lines[11].split('value="')[1].split('"')[0]
+	id_lifras = int(form_lines[11].split('value="')[1].split('"')[0])
 	ice = result.text.split("ICE <b>")[1].split("</b>")[0]
 	aig = result.text.split(" AIG Call center ")[1].split("<br>")[0]
 	police = result.text.split(" AIG Call center ")[1].split("<br>Police ")[1].split("</b>")[0]
 	text = result.text.split("fa-medkit")[1].split("</i>")[1].split("</div>")[0]
 	if "Medical situation in order !" in text:
 		medic_status = True
-		medic_date = dt.strptime(text.split('Valid to ')[1], '%d/%m/%Y')
+		medic_date = dt.strptime(text.split('Valid to ')[1], '%d/%m/%Y').date()
 	else:
 		medic_status = False
 		medic_date = None
@@ -49,7 +49,7 @@ def whois(nom, prenom="", dna="", id_lifras=""):
 	text = result.text.split("fa-heartbeat")[1].split("</i>")[1].split("</div>")[0]
 	if " ECG in order !" in text:
 		ecg_status = True
-		ecg_date = dt.strptime(text.split('Valid to ')[1], '%d/%m/%Y')
+		ecg_date = dt.strptime(text.split('Valid to ')[1], '%d/%m/%Y').date()
 	else:
 		ecg_status = False
 		ecg_date = None
